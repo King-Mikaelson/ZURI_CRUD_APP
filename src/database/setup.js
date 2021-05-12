7681197de93762d7849aafed710c27225f4c01af
King-Mikaelson <anazodomichael27@gmail.com>
2021-05-12 10:13:05 -0700

    okay

--- src/database/setup.js
+++ src/database/setup.js
@@ -1,7 +1,7 @@
 // SET UP MONGOOSE STILL GOING TO MOVE THIS TO A NEW FOLDER SETUP
 const mongoose = require("mongoose");
-const connectionString = "mongodb://localhost:27017/Zuriapp";
-
+// const connectionString = "mongodb://localhost:27017/Zuriapp";
+const connectionString = "mongodb+srv://michealjackson:michealjackson@cluster0.s0syo.mongodb.net/Zuriapp?retryWrites=true&w=majority"
 module.exports = () => {
     mongoose.connect(connectionString, {
         useNewUrlParser : true,
